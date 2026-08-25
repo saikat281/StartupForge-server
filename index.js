28154cbfdc20b5cbdeca1a8ccea904065a8c0e81
Saikat Sarkar <sarkarsaikat3467@gmail.com>
2026-08-25 14:42:14 +0600

    Added CORS origin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors")
 
 dotenv.config();
 
@@ -10,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
 
 // Middleware
+app.use(cors())
+
 app.use(express.json());
+
+
 
 // MongoDB client
 const client = new MongoClient(uri, {
